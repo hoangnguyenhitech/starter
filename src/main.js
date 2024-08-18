5395cbfc213ea53586ab922b597a9aa544286998
H O A N G N G U Y E N <hoangnp@devblock.net>
2024-08-18 14:51:10 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,26 +1,19 @@
-import { Client } from 'node-appwrite';
+import AppwriteService from './appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
-
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
+  const appwrite = new AppwriteService();
+  log(req);
+  log(res);
   error('Hello, Errors!');
-
   // The `req` object contains the request data
   if (req.method === 'GET') {
+    console.log('req:', req);
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+    const list = await appwrite.getAllListDoc('restaurants');
+    return res.send(list);
   }
 
   // `res.json()` is a handy helper for sending JSON
