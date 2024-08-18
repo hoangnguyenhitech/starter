462e288227ee7b8611f95e6d291ad4cf6c4fe725
Appwrite <team@appwrite.io>
2024-08-18 08:08:34 +0000

    Create 'get Phone zalo' function

--- src/main.js
+++ src/main.js
@@ -1,19 +1,26 @@
-import AppwriteService from './appwrite';
+import { Client } from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  const appwrite = new AppwriteService();
-  log(req);
-  log(res);
+  // Why not try the Appwrite SDK?
+  //
+  // const client = new Client()
+  //    .setEndpoint('https://cloud.appwrite.io/v1')
+  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+  //    .setKey(process.env.APPWRITE_API_KEY);
+
+  // You can log messages to the console
+  log('Hello, Logs!');
+
+  // If something goes wrong, log an error
   error('Hello, Errors!');
+
   // The `req` object contains the request data
   if (req.method === 'GET') {
-    console.log('req:', req);
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
-    const list = await appwrite.getAllListDoc('restaurants');
-    return res.send(list);
+    return res.send('Hello, World!');
   }
 
   // `res.json()` is a handy helper for sending JSON
